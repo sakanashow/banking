06a52033a0e806096bc016e9a80121b68bbf52b0
shotam <mashou625@gmail.com>
2024-08-07 17:17:24 +0900

    Auth Pageの途中（２）

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,11 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const SignIN = () => {
   return (
-    <div>SignIN</div>
+    <section className='flex-center size-full max-sm:px-6'>
+      <AuthForm type="sign-in" />
+    </section>
   )
 }
 
